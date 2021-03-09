2677df2b0415d54650e30b5ef61c23a5d83beb46
Marco Aurelio Simoes Schmidt Junior <Marco.Junior@ibm.com>
2021-03-09 16:26:11 -0300

    hello world script

---
+++ java_basics/src/java_basics/Hello.java
@@ -0,0 +1,15 @@
+package java_basics;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// comments use // by default
+		
+		// to print something to the screen you should do as following
+		System.out.println("Hello World");
+		
+		System.out.println("Well the weather is great");
+	}
+	// this is a HELLO WORLD  script for javax
+
+}
