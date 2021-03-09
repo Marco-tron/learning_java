9717ab1bb2175b5c7f7db63860e9f1d7ad9303eb
Marco Aurelio Simoes Schmidt Junior <Marco.Junior@ibm.com>
2021-03-09 16:46:03 -0300

    variables and datatypes basics

---
+++ java_basics/src/java_basics/Variables.java
@@ -0,0 +1,30 @@
+package java_basics;
+
+public class Variables {
+// Let's talk about variables now
+	public static void main(String[] args) {
+		//  before using a variable you must declare it with a type, like int for integer
+		int x;
+		
+		
+		//with this you are assigning a value to the variable declared earlier
+		x = 4;
+		System.out.println(x);
+		
+		//String is a datatype commonly used in languages to store words and sentences
+		String words;
+		
+		words = "basic sentence in a string";
+		
+		System.out.println(words);
+		
+		// you can also declare and assign on the same line
+		
+		int y = 20;
+		
+		
+		System.out.println(y+x);
+
+	}
+
+}
