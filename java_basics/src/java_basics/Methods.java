05ef8fe3b4a8f65f35b274d33fb2a4b7d92eae98
Marco Aurelio Simoes Schmidt Junior <Marco.Junior@ibm.com>
2021-03-10 14:09:58 -0300

    methods in java

---
+++ java_basics/src/java_basics/Methods.java
@@ -0,0 +1,36 @@
+package java_basics;
+
+public class Methods {
+
+	public static void main(String[] args) {
+		//println is an method that receives data and prints the data to the screen
+		System.out.println("Hello there");
+		// calling the printing method you estabilished outside of main
+		Printing();
+		PrintingWithVariables("I am pasting this data");
+		
+		Utils.UtilsPrinting("Hey theree, I'm using a method from another class");
+		
+		
+		//you can also make a function return some value to be used by other functions
+		System.out.println(Utils.add10(25));
+		
+	}
+	// here you are defining a method that will be called by the main method
+	public static void Printing() {
+	System.out.println("printing something");
+	
+	}
+	
+	// you can also pass variables to methods
+	public static void PrintingWithVariables(String a) {
+		System.out.println(a);
+		
+		}
+	// you can have more than one method with them accepting different variable types
+	public static void PrintingWithVariables(int a) {
+		System.out.println("Pasting an Integer: "+a);
+		
+		}
+}
+
---
+++ java_basics/src/java_basics/Utils.java
@@ -0,0 +1,14 @@
+package java_basics;
+
+public class Utils {
+		public static void UtilsPrinting(String a) {
+			System.out.println(a);
+			
+			}
+		
+		//you can also make methods return values
+		public static int add10(int Argument) {
+			int result = Argument+10;
+			return result;
+		}
+}
